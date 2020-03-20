8cf17fa21d4142d8f26ebd6dd8f58a7364b2b40b
MarceloLudovino <ext-m.ludovino@rappi.com>
2020-03-20 17:16:32 -0300

    Add friction

--- Assets/Chara/Movements.cs
+++ Assets/Chara/Movements.cs
@@ -6,6 +6,7 @@ namespace Chara
     {
         public float playerSpeed;
         private Rigidbody2D _rb2d;
+        public float friction; 
 
         // Start is called before the first frame update
         void Start()
@@ -16,6 +17,8 @@ namespace Chara
         // Update is called once per frame
         void Update()
         {
+            Deceleration();
+
             if (Input.GetKey(KeyCode.D))
             {
                 _rb2d.AddForce(new Vector2(playerSpeed,0));
@@ -37,6 +40,31 @@ namespace Chara
             if (_rb2d.velocity.x < -5)
             {
                 _rb2d.velocity = new Vector2(-5, _rb2d.velocity.y);
+            }
+        }
+
+        private void Deceleration()
+        {
+            var deceleration = 0f;
+
+            if(_rb2d.velocity.x > 0)
+            {
+                deceleration = _rb2d.velocity.x - (friction * Time.deltaTime);
+                if(deceleration < 0)
+                {
+                    deceleration = 0;
+                }
+                _rb2d.velocity = new Vector2(deceleration, _rb2d.velocity.y);
+            }
+
+            if (_rb2d.velocity.x < 0)
+            {
+                deceleration = _rb2d.velocity.x + (friction * Time.deltaTime);
+                if (deceleration > 0)
+                {
+                    deceleration = 0;
+                }
+                _rb2d.velocity = new Vector2(deceleration, _rb2d.velocity.y);
             }
         }
     }
